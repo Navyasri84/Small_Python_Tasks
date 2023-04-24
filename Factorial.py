a8bd39f3db766e75a8ad95037b86635428288e86
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2023-04-24 21:28:29 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         r = n*factorial(n-1)
+#         return r
+# n=int(input("Enter the num: "))
+# print(f"{n}!=",factorial(n))
+
+"or"
+
+# import math
+# n = int(input("Enter a num: "))
+# print(f"{n}! =",math.factorial(n))
